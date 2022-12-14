f520e3cb32cfe59330f85a16e9b062b3e5858705
Altobert <alberto.sanmartin@usach.cl>
2022-12-14 00:08:25 -0300

    modificaciones

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 
+{/*funcion para almacenamiento local*/}
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key)
